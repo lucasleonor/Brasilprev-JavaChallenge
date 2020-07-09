bd9f1d9df57da2837d2310f21f38f3bb73ebb14a
lgleonor <lgleonor@marketdata.com.br>
2020-07-08 21:18:21 -0300

    Removed Basket, renamed BasketItem to OrderItem

--- src/main/java/br/com/brasilprev/teste/javachallenge/dao/BasketDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.brasilprev.teste.javachallenge.dao;
-
-import br.com.brasilprev.teste.javachallenge.model.Basket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BasketDao extends JpaRepository<Basket, Integer> {
-}
---
+++ src/main/java/br/com/brasilprev/teste/javachallenge/model/OrderItem.java
@@ -0,0 +1,43 @@
+package br.com.brasilprev.teste.javachallenge.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "amount")
+    private Integer amount;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @Getter(onMethod = @__(@JsonIgnore))
+    private Order order;
+}
